0c8fff5fa098bbf2ec7a3752662346226e15d651
LAMAZENODE <salimi.lina@yahoo.fr>
2026-09-06 15:34:15 +0200

    Refactor simulateur_pro.py for improved structure

--- simulateur_pro.py
+++ simulateur_pro.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="IA & Expertise Patrimoniale", layout="wide")
 
 # --- PARAMÈTRE À REMPLIR ---
 # Collez ici votre vrai lien de paiement Stripe créé sur votre tableau de bord
-LIEN_PAIEMENT_STRIPE = "https://stripe.com" 
+LIEN_PAIEMENT_STRIPE = "https://buy.stripe.com/dRm3cu0zBgqm4V66ta8g003" 
 
 # Initialisation des états de session
 if "paiement_reussi" not in st.session_state:
